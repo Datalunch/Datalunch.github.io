ec57780720859ceb43f4b789ae6021ae7b2bd89a
Datalunch <pbirrittella@Pauls-MacBook-Air.local>
2017-09-11 14:57:49 -0600

    Revamped Navigation bar, removed one page

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+
+
+//Copyright current year
+
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById('copyright');
+el.innerHTML= '&copy; ' + year + ' 460bread.com';
